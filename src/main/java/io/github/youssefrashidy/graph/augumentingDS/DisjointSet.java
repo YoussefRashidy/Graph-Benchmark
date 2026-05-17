9730f253144f44c3e76c14c9b46e289dcaf5f390
Youssef Ali Rashidi Ali <es.yousef.rashidy2023@alexu.edu.eg>
2026-05-17 21:32:23 +0300

    Implement Disjoint Set data structure with makeSet, findSet, and union methods

--- src/main/java/io/github/youssefrashidy/graph/augumentingDS/DisjointSet.java
+++ src/main/java/io/github/youssefrashidy/graph/augumentingDS/DisjointSet.java
@@ -1,4 +1,44 @@
 package io.github.youssefrashidy.graph.augumentingDS;
 
+import org.eclipse.collections.impl.map.mutable.primitive.IntIntHashMap;
+
 public class DisjointSet {
+    IntIntHashMap parent = new IntIntHashMap();
+    IntIntHashMap rank = new IntIntHashMap();
+
+    public void makeSet(int vertex) {
+        if (parent.containsKey(vertex))
+            return;
+        parent.put(vertex, vertex);
+        rank.put(vertex, 0);
+    }
+
+    public int findSet(int vertex) {
+        int parent = this.parent.get(vertex);
+
+        if (parent != vertex) {
+            this.parent.put(vertex, findSet(parent));
+        }
+
+        return this.parent.get(vertex);
+    }
+
+    public void union(int u, int v) {
+        int uParent = findSet(u);
+        int vParent = findSet(v);
+        if (uParent == vParent)
+            return;
+        int uRank = rank.get(uParent);
+        int vRank = rank.get(vParent);
+
+        if (uRank > vRank)
+            parent.put(vParent, uParent);
+        else if (vRank > uRank)
+            parent.put(uParent, vParent);
+        else {
+            parent.put(vParent, uParent);
+            rank.put(uParent, uRank + 1);
+        }
+    }
+
 }
